c2e5443f9c0a7d4887df41f29ad9a78d7423580d
ebraimsambo <ebraimsambo@gmail.com>
2024-07-22 23:50:22 +0100

    boo

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RefreshJwtGuard } from './guard/refresh.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService){}
+
+    @Post("login")
+    login(@Body() loginDto: LoginDto){
+        return this.authService.login(loginDto)
+    }
+
+    @UseGuards(RefreshJwtGuard)
+    @Post('refresh')
+    async refreshToken(@Request() req) {
+      console.log('refreshed');
+  
+      return await this.authService.refreshToken(req.user);
+    }
+}
